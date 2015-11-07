ffd6eb43089e12115dc98e8e25e0e695d198582d
nidhisha-shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2015-11-06 20:00:00 -0600

    Pushing book repository to GitHub

---
+++ books/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+	book_title = models.CharField(max_length=100)
+	book_author = models.CharField(max_length=50)
+	book_description=models.TextField(blank=True, null=True)
+	book_img = models.ImageField(upload_to='images/', blank=False) 
+
+	def go_to_link_edit(self):
+		return f"/edit/{self.id}/"
+
+	def go_to_link_view(self):
+		return f"/view/{self.id}"
+
+	def go_to_link_delete(self):
+		return f"/delete/{self.id}"
